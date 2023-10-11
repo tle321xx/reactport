4f27a3c9c7485aa23508d3da21420cf4ba828e29
tle321xx <t.wachirawit28@gmail.com>
2023-10-11 15:54:24 +0700

    make it better

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,55 +3,100 @@ import './Home.scss'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import Loader from 'react-loaders'
+import profile from '../../assets/images/profile.jpg'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = ['','w', 'a', 'c', 'h', 'i', 'r', 'a', 'w', 'i', 't']
-  const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.',]
-  const welcome = ['W','e','l','c','o','m','e','','t','o','','o','u','r','','w','e','b','s','i','t','e','!']
+  const nameArray = ['', 'w', 'a', 'c', 'h', 'i', 'r', 'a', 'w', 'i', 't']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+  const welcome = [
+    'W',
+    'e',
+    'l',
+    'c',
+    'o',
+    'm',
+    'e',
+    '',
+    't',
+    'o',
+    '',
+    'o',
+    'u',
+    'r',
+    '',
+    'w',
+    'e',
+    'b',
+    's',
+    'i',
+    't',
+    'e',
+    '!',
+  ]
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLetterClass('text-animate-hover');
-    }, 4000);
-      return () => clearTimeout(timer);
-  }, []);
-  
+      setLetterClass('text-animate-hover')
+    }, 4000)
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
-    <><Loader type="pacman"/>
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-        <AnimatedLetters
-            letterClass={letterClass}
-            strArray={welcome}
-            idx={13}
-          />
-          <br />
-          <span className={letterClass}>H</span>
+    <>
+      <Loader type="pacman" />
+      <div className="container home-page">
+      <img src={profile} alt='profile-img' />
+
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={welcome}
+              idx={13}
+            />
+            
+            <br />
+            <span className={letterClass}>H</span>
             <span className={`${letterClass} _12`}>i,</span>
             <br />
             <span className={`${letterClass} _13`}>I</span>
             <span className={`${letterClass} _14`}>'m</span>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>Junior Software Developer / JavaScript / React</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Junior Software Developer / JavaScript / React</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+          
+
+        </div>
       </div>
-    </div>
     </>
   )
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './Sidebar.scss'
 import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope, faSuitcase } from '@fortawesome/free-solid-svg-icons'
 import {
@@ -15,7 +14,6 @@ const Sidebar = () => {
     <div className="nav-bar">
       <Link className="logo" to="/">
         <img src={LogoS} alt="logo" />
-        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
       </Link>
       <nav>
         <NavLink exact="true" activeclassname="active" to="/">
