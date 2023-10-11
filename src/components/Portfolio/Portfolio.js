0583bfb34c4e113f85836e38ab4e1d70f7af5e8a
tle321xx <t.wachirawit28@gmail.com>
2023-10-11 16:15:10 +0700

    fix 4

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -22,12 +22,12 @@ const Portfolio = () => {
                     portfolio.map((port, idx)=>{
                         return  (
                             <div className="image-box" key={idx}>
-                                <img src={port.cover} alt="portfolio" className="portfolio-image"/>
+                                {/* <img src={port.cover} alt="portfolio" className="portfolio-image"/>
                                 <div className="content">
                                     <p className="title">{port.title}</p>
                                     <h4 className="description">{port.description}</h4>
                                     <button className='btn' onClick={()=> window.open(port.url)}>view</button>
-                                </div>
+                                </div> */}
                             </div>
                         )
                     })
