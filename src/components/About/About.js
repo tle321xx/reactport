bf76d841be0d2a300956490057c015fe8535cd4b
tle321xx <t.wachirawit28@gmail.com>
2023-10-11 16:25:02 +0700

    fix 5

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,17 +3,16 @@ import React from 'react'
 import './About.scss'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import {
-    faGithub,
-    faCss3,
-    faGitAlt,
-    faHtml5,
-    faJsSquare,
-    faReact,
-  } from '@fortawesome/free-brands-svg-icons'
-  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import Loader from 'react-loaders'
+  faGithub,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Loader from 'react-loaders'
 
-  
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
@@ -26,31 +25,33 @@ const About = () => {
 
   return (
     <>
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', '', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          I'm a very ambitious front-end developer looking for a role in an
-          established IT company with the opportunity to work with the latest
-          technologies on challenging and diverse projects.
-        </p>
-        <p align="LEFT">
-          I'm quiet confident, naturally curious, and perpetually working on
-          improving my chops one design problem at a time.
-        </p>
-        <p>
-          If I need to define myself in one sentence that would be a family
-          person, father of a beautiful daughter, a sports fanatic, photography
-          enthusiast, and tech-obsessed!!!
-        </p>
-      </div>
-      <div className="stage-cube-cont">
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', '', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I graduated from the Civil Aviation Training Center and have been
+            dedicatedly learning the MERN stack for the past 8 months. My
+            passion for software development drives me to continuously enhance
+            my skills in this field.
+          </p>
+          <p>
+            I'm a very ambitious front-end developer looking for a role in an
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          <p align="LEFT">
+            I'm quiet confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p>
+         
+        </div>
+        <div className="stage-cube-cont">
           <div className="cubespinner">
             <div className="face1">
               <FontAwesomeIcon icon={faGithub} color="black" />
@@ -72,8 +73,8 @@ const About = () => {
             </div>
           </div>
         </div>
-    </div>
-    <Loader type="pacman"/>
+      </div>
+      <Loader type="pacman" />
     </>
   )
 }
